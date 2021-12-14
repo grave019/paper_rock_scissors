09eb316c1aeb0b19a4749aa39e6ff3ae02cbade7
Brent Graves <graves.brent@gmail.com>
2021-12-14 13:06:42 -0500

    if user presses cancel function ends.

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ var losses = 0;
 
 //options for the computer to pick from
 var options = ["R", "P", "S"];
+
+var startGame = function() {
+    // Ask the user to choose R, P or S
+    var userChoice = window.prompt("Enter R, P, or S:");
+
+     // If user presses Cancel, immediately end function
+  if (!userChoice) {
+    return;
+  }
